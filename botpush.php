7cf289ce04d3b1050fe8a342999cd5389dcd129c
montree51139 <45867734+montree51139@users.noreply.github.com>
2018-12-14 19:14:44 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -1,7 +1,7 @@
 <?php
 
 
-
+<form action="/action_page.php" method="get">
 require "vendor/autoload.php";
 
 $access_token = 'NVUaJzyIa7s4wNxVEE0VNnZqxV8soHPZa7qPIqmdwdRk1IKB55wFYAD2H1PwyUDF81XDGjrVlHtzThJeWWrMjPwLLggGRUNFr04xXyHVWFBRQJDTslKwrhCrErj0gUvN7nAYMFwqyWCuDPvnJ0qsVQdB04t89 / 1O / w1cDnyilFU =';
@@ -13,11 +13,11 @@ $pushID = 'Ueac469ec6c9f1c655acd64434090854c';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+<form action="/action_page.php" method="get">$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');</form>
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-
+</form>
 
 
 
