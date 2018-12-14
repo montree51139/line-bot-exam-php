31246255431726b3b491655c043c6248bb9ef132
montree51139 <45867734+montree51139@users.noreply.github.com>
2018-12-14 19:40:18 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -13,7 +13,7 @@ $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
-<button type="submit"$response = $bot->pushMessage($pushID, $textMessageBuilder);value="Submit">Submit</button>
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
